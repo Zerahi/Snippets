13686f6cbea8bfe9c2e439b0bd4e1126e482716c
Zerahi <nathan_nsk@hotmail.com>
2017-12-18 23:42:32 -0600

    Update SortNumbersbyvalue.vb

--- SortNumbersbyvalue.vb
+++ SortNumbersbyvalue.vb
@@ -3,15 +3,15 @@ Dim z = 1
 Dim y = Lists.Count - 1
 If y > 0 Then
 While x < y
-If CInt(Lists(x)) > CInt(Lists(z)) Then
-Dim tmp = Lists(z)
-Lists(z) = Lists(x)
-Lists(x) = tmp
-z = x + 1
-End If
-If z = y Then
-x += 1
-z = x
-End If
-z += 1
+  If CInt(Lists(x)) > CInt(Lists(z)) Then
+    Dim tmp = Lists(z)
+    Lists(z) = Lists(x)
+    Lists(x) = tmp
+    z = x + 1
+  End If
+  If z = y Then
+    x += 1
+    z = x
+  End If
+  z += 1
 End While
